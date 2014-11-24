2ec3976899b855d11ad99024f2af1bca328b787b
Alex van der Linden <alexvanderlinden@me.com>
2014-11-24 01:19:50 +0100

    USB Serial Connection "tx: test"

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
+
+#include "stm32_ub_usb_cdc.h"
+
 int main(void)
 {
-	PLLInit();
+  PLLInit();
 
-	while(1)
-    {
+  UB_USB_CDC_Init(); // init
+while(1){
+	  // check if USB connected
+	  if(UB_USB_CDC_GetStatus()==USB_CDC_CONNECTED) {
+		// send String to USB (+CarriageReturn+LineFeed)
+		UB_USB_CDC_SendString("Test", CRLF);
+	  }
+}
 
-    }
 }
