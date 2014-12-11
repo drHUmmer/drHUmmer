ea84f9840af01fac3bef026375420af96d817ab5
jantrompper <jantrompper@gmail.com>
2014-12-11 12:51:06 +0100

    All midi functions are BACK!

--- midi.c
+++ midi.c
@@ -55,10 +55,116 @@ uint8_t MidiSend(MIDImsg_t *msg, uint8_t ch)
 }
 
 /* MidiNoteOn()
+ * Sends a MIDI note on message
  *
+ * Usage:
+ * MidiNoteOn(note, channel, velocity);
  */
-uint8_t MidiNoteOn()
+uint8_t MidiNoteOn(notes note, uint8_t ch, uint8_t vel)
 {
+	MIDImsg_t midiMsg;
 
+	midiMsg.head = (NOTE_ON|ch);
+	midiMsg.msg1 = note;
+	midiMsg.msg2 = vel;
+
+	MidiSend(&midiMsg, ch);
+
+	return 0;
+}
+
+/* MidiNoteOff()
+ * Sends a MIDI note off message
+ *
+ * Usage:
+ * MidiNoteOff(note, channel, velocity);
+ */
+uint8_t MidiNoteOff(notes note, uint8_t ch, uint8_t vel)
+{
+	MIDImsg_t midiMsg;
+
+	midiMsg.head = (NOTE_OFF|ch);
+	midiMsg.msg1 = note;
+	midiMsg.msg2 = vel;
+
+	MidiSend(&midiMsg, ch);
+
+	return 0;
+}
+
+/* MidiClock()
+ * Sends a MIDI clock message
+ *
+ * Usage:
+ * MidiClock();
+ */
+uint8_t MidiClock(void)
+{
+	MIDImsg_t midiMsg;
+
+	midiMsg.head = MIDI_CLOCK;
+	midiMsg.msg1 = 0;
+	midiMsg.msg2 = 0;
+
+	MidiSend(&midiMsg, MIDI_ALL_CH);
+
+	return 0;
+}
+
+/* MidiStart()
+ * Sends a MIDI start message
+ *
+ * Usage:
+ * MidiStart();
+ */
+uint8_t MidiStart(void)
+{
+	MIDImsg_t midiMsg;
+
+	midiMsg.head = MIDI_START;
+	midiMsg.msg1 = 0;
+	midiMsg.msg2 = 0;
+
+	MidiSend(&midiMsg, MIDI_ALL_CH);
+
+	return 0;
+}
+
+/* MidiStop()
+ * Sends a MIDI stop message
+ *
+ * Usage:
+ * MidiStop();
+ */
+uint8_t MidiStop(void)
+{
+	MIDImsg_t midiMsg;
+
+	midiMsg.head = MIDI_STOP;
+	midiMsg.msg1 = 0;
+	midiMsg.msg2 = 0;
+
+	MidiSend(&midiMsg, MIDI_ALL_CH);
+
+	return 0;
+}
+
+/* MidiContinue()
+ * Sends a MIDI continue message
+ *
+ * Usage:
+ * MidiContinue();
+ */
+uint8_t MidiContinue(void)
+{
+	MIDImsg_t midiMsg;
+
+	midiMsg.head = MIDI_CONT;
+	midiMsg.msg1 = 0;
+	midiMsg.msg2 = 0;
+
+	MidiSend(&midiMsg, MIDI_ALL_CH);
+
+	return 0;
 }
 
--- midi.h
+++ midi.h
@@ -20,6 +20,7 @@
 #define MIDI_CONT	0xFB
 
 //midi channels, mind the offset, channel 1 = 0x0
+#define MIDI_ALL_CH	0x00
 #define MIDI_CH1 	0x00
 #define MIDI_CH2 	0x01
 #define MIDI_CH3 	0x02
@@ -59,11 +60,35 @@ typedef struct
 	uint8_t msg2;
 }MIDImsg_t;
 
+enum notes
+{
+	C0, C_0, D0, D_0, E0, F0, F_0, G0, G_0, A0, A_0, B0,
+	C1, C_1, D1, D_1, E1, F1, F_1, G1, G_1, A1, A_1, B1,
+	C2, C_2, D2, D_2, E2, F2, F_2, G2, G_2, A2, A_2, B2,
+	C3, C_3, D3, D_3, E3, F3, F_3, G3, G_3, A3, A_3, B3,
+	C4, C_4, D4, D_4, E4, F4, F_4, G4, G_4, A4, A_4, B4,
+	C5, C_5, D5, D_5, E5, F5, F_5, G5, G_5, A5, A_5, B5,
+	C6, C_6, D6, D_6, E6, F6, F_6, G6, G_6, A6, A_6, B6,
+	C7, C_7, D7, D_7, E7, F7, F_7, G7, G_7, A7, A_7, B7,
+	C8, C_8, D8, D_8, E8, F8, F_8, G8, G_8, A8, A_8, B8,
+	C9, C_9, D9, D_9, E9, F9, F_9, G9, G_9, A9, A_9, B9
+};
+
 /*VARIABLES*/
 extern uint16_t midiStatus;
 extern MIDImsg_t midiTest;
 
 /*FUNCTION PROTOTYPES*/
 uint8_t MidiInit(uint8_t mode);
+uint8_t MidiSend(MIDImsg_t *msg, uint8_t ch);
+uint8_t MidiNoteOn(notes note, uint8_t ch, uint8_t vel);
+uint8_t MidiNoteOff(notes note, uint8_t ch, uint8_t vel);
+uint8_t MidiClock(void);
+uint8_t MidiStart(void);
+uint8_t MidiStop(void);
+uint8_t MidiContinue(void);
+
+
+
 
 #endif
